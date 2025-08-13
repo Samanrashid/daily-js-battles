12fe297fbe635d0a53c39355e20a814c57ef7a02
Samaneasyeat <156658674+Samaneasyeat@users.noreply.github.com>
2025-08-13 21:28:06 +0530

    reverse array leetcode

--- 01_array.js
+++ 01_array.js
@@ -1,1 +1,10 @@
 // demo commit
+// Reverse an array without using reverse
+function reverseArray(arr) {
+  let reverseArray = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reverseArray.push(arr[i]);
+  }
+  return reverseArray;
+}
+console.log(reverseArray([2, 3, 1]));
