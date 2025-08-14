2b59c419a7c5e5fe2ed25147dfa2b2bbdcf7fb0e
Samaneasyeat <156658674+Samaneasyeat@users.noreply.github.com>
2025-08-14 13:52:23 +0530

    find occuracy and given dataset

---
+++ 06_array.js
@@ -0,0 +1,7 @@
+//count occuracy in given array
+let fruits = ["apple", "apple", "banana", "apple", "banana"];
+const count = fruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0) + 1;
+  return acc;
+}, {});
+console.log(count);
